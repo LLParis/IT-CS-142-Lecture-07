cdec2e86fd7081881bc73461c648964c073d1b6c
Your Name <you@example.com>
2024-02-07 01:15:20 -0800

    Initial Commit

---
+++ src/Lecture07.java
@@ -0,0 +1,22 @@
+public class Lecture07 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    // SELF-CHECK PROBLEMS:
+    // Note: you will need to put some of your answers inside a comments
+
+    // Put your answer for #3 here:
+
+
+    // Put your answer for #5 here:
+    // (The answer will be one of "A" or "B" or "C" or "D" or "E")
+
+    // Put your answer for #7 here:
+
+    // EXERCISES:
+
+    // Put your answer for #1 here:
+    // You will need to add the method in above main(), but then call it here
+  }
+}
